a8abf46b8dcec8f75dd1678397b568fb3a19eaec
Anselmo Henrique <100934388+Anselmo5@users.noreply.github.com>
2022-09-06 12:25:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("btn-vermelho").addEventListener("click", function () {
+    document.querySelector("body").setAttribute("class","vermelho");
+})
+
+document.getElementById("btn-verde").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","verde");
+})
+
+document.getElementById("btn-azul").addEventListener("click", function(){
+    document.querySelector("body").setAttribute("class","azul");
+})
